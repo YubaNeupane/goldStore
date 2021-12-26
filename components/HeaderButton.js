3b826fdb51d8e0f4115e1d2bebe3729692e56975
YubaNeupane <neopaneyb2@gmail.com>
2021-12-26 16:49:24 -0500

    added number top of shopping cart

--- components/HeaderButton.js
+++ components/HeaderButton.js
@@ -1,18 +1,46 @@
 import React from "react";
-import { StyleSheet, Platform } from "react-native";
+import { StyleSheet, Platform, View, Text } from "react-native";
 import { HeaderButton } from "react-navigation-header-buttons";
 import { Ionicons } from "@expo/vector-icons";
 import Colors from "../constants/Colors";
 
 export default function CustomHeaderButton(props) {
+  const color = Platform.OS == "android" ? "blue" : "red";
   return (
-    <HeaderButton
-      {...props}
-      IconComponent={Ionicons}
-      iconSize={30}
-      color={Platform.OS == "android" ? "red" : "blue"}
-      // color={Platform.OS == "android" ? "white" : Colors.secondaryDarkColor}
-    />
+    <View>
+      <HeaderButton
+        {...props}
+        IconComponent={Ionicons}
+        iconSize={30}
+        color={Platform.OS == "android" ? "red" : "blue"}
+        // color={Platform.OS == "android" ? "white" : Colors.secondaryDarkColor}
+      />
+      <View
+        style={{
+          position: "absolute",
+          backgroundColor: color,
+          width: 16,
+          height: 16,
+          borderRadius: 15 / 2,
+          right: 10,
+          top: -3,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            color: "white",
+            fontSize: 10,
+            fontWeight: "bold",
+          }}
+        >
+          {6}
+        </Text>
+      </View>
+    </View>
   );
 }
 
