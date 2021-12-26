e83982d439b21ed4eb1c39f133a8371b5724e62d
charlie <charlie@charlies-MacBook-Pro-2.local>
2021-12-25 21:53:21 -0500

    added the categogries page

---
+++ data/CategoriesName.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id:1,
+        title: "Bracelet",
+        image:"https://m.media-amazon.com/images/I/71wWrfV4fnL._SL1500_.jpg",
+    },
+    {
+        id:2,
+        title: "Choker Haar",
+        image:"https://m.media-amazon.com/images/I/71wWrfV4fnL._SL1500_.jpg",
+    },
+    {
+        id:3,
+        title: "Chandra Haar",
+        image:"https://m.media-amazon.com/images/I/71wWrfV4fnL._SL1500_.jpg",
+    },
+    {
+        id:4,
+        title: "Chain (sikri)",
+        image:"https://m.media-amazon.com/images/I/71wWrfV4fnL._SL1500_.jpg",
+    },
+    {
+        id:5,
+        title: "Wedding Set",
+        image:"https://m.media-amazon.com/images/I/71wWrfV4fnL._SL1500_.jpg",
+    },
+    {
+        id:6,
+        title: "Raani Haar",
+        image:"https://m.media-amazon.com/images/I/71wWrfV4fnL._SL1500_.jpg",
+    },
+    {
+        id:7,
+        title: "Step Tilhari",
+        image:"https://m.media-amazon.com/images/I/71wWrfV4fnL._SL1500_.jpg",
+    },
+
+]
