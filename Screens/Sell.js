dabecc9f3052f87cd03b57c1a0bde27c802c78ed
charlie <charlie@charlies-MBP-2.fios-router.home>
2021-12-27 13:46:07 -0500

    Works on sell page

--- Screens/Sell.js
+++ Screens/Sell.js
@@ -1,18 +1,53 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import Colors from "../constants/Colors";
+import Card from "../components/Card";
+import HeadingTitle from "../components/HeadingTitle";
+import GoldPrice from "../components/GoldPrice";
 
 export default function Sell() {
+  const getGoldPrice = 10.99;
+  const sellGoldPrice = getGoldPrice - 2.00;
+  const inputValue = 6;
   return (
     <View style={styles.screen}>
-      <Text></Text>
+      <View>
+      <Card style={styles.card}>
+      <HeadingTitle>Want to Sell Your Gold?</HeadingTitle>
+      <Text style={styles.text}>Calcuate the Price below</Text>
+      <Card>
+        <TextInput placeholder="Enter Weight in Grams" keyboardType ={"numeric"}></TextInput>
+      </Card>
+      <View>
+        <Text style={styles.text}>Total Price: </Text>
+        <Text style={styles.sellPrice}>{sellGoldPrice * inputValue}</Text>
+        </View>
+      </Card>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1,
-    justifyContent: "center",
+    flex:1,
     alignItems: "center",
+    backgroundColor: Colors.backgroundViewColor,
   },
+  card: {
+    backgroundColor: Colors.primaryDarkColor,
+  },
+  text: {
+    fontSize: 10,
+    fontFamily: "cinzel-semiBold",
+    color: Colors.secondaryTextColor,
+  },
+  sellPrice: {
+    flexDirection: "row",
+    fontSize: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "cinzel-semiBold",
+    color: Colors.secondaryTextColor,
+  }
 });
