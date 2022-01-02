f34f610561bdfb97ae5d354488e2ee6c7e2db10c
YubaNeupane <neopaneyb2@gmail.com>
2022-01-02 15:36:18 -0500

    custom button finished

--- components/CustomAddToCartButton.js
+++ components/CustomAddToCartButton.js
@@ -1,12 +1,42 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Color from "../constants/Colors";
 
 export default function CustomAddToCartButton() {
   return (
-    <View>
-      <Text>Add To Cart</Text>
-    </View>
+    <TouchableOpacity>
+      <View style={styles.container}>
+        <View>
+          <MaterialCommunityIcons
+            name="cart-plus"
+            size={42}
+            color={Color.secondaryTextColor}
+          />
+        </View>
+        <View>
+          <Text style={styles.text}>Add To Cart</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: Color.primaryLightColor,
+    padding: 10,
+    marginTop: -50,
+    marginBottom: 10,
+    width: 250,
+    borderRadius: 100,
+  },
+  text: {
+    fontFamily: "cinzel-bold",
+    fontSize: 20,
+    color: Color.primaryDarkColor,
+  },
+});
