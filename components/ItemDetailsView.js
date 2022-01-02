efff7a7daecdb705fa089dc06fa138271b3f7d31
YubaNeupane <neopaneyb2@gmail.com>
2022-01-02 14:02:03 -0500

    big text

--- components/ItemDetailsView.js
+++ components/ItemDetailsView.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
-
+import { useSelector } from "react-redux";
 const { width, height } = Dimensions.get("window");
 
+function currencyFormat(num) {
+  return "$" + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+}
+
 export default function ItemDetailsView({ item }) {
+  const goldPriceT = useSelector((state) => state.goldPrice);
+  const getCurrentGoldPrice = goldPriceT.price;
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
@@ -11,7 +17,12 @@ export default function ItemDetailsView({ item }) {
       </View>
       <View style={styles.infoContainer}>
         <Text style={styles.infoTitle}>WEIGHT: {item.weight}</Text>
-        <Text style={styles.infoTitle}>PRICE: {item.price}</Text>
+        <Text style={styles.infoTitle}>
+          PRICE:{" "}
+          {currencyFormat(
+            parseInt(item.price) + parseInt(item.weight) * getCurrentGoldPrice
+          )}
+        </Text>
       </View>
     </View>
   );
@@ -26,12 +37,15 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     alignItems: "center",
-    marginVertical: 10,
+    marginVertical: 0,
   },
   title: {
-    fontSize: 30,
+    fontSize: 35,
   },
   infoContainer: {
     padding: 25,
+  },
+  infoTitle: {
+    fontSize: 20,
   },
 });
